fc3c3ef66c60a6b4f0156ec4761b90bdfe40279a
Suho Kwon <suho.kwon@gmail.com>
2022-07-03 15:01:37 +0900

    Add commments example

---
+++ comments/target/debug/comments.d
@@ -0,0 +1,1 @@
+/Users/suhokwon/Projects/Rust/comments/target/debug/comments: /Users/suhokwon/Projects/Rust/comments/src/main.rs
---
+++ hellocargo/target/debug/deps/hellocargo-f5b6d01e6c1e54a6.d
@@ -0,0 +1,5 @@
+/Users/suhokwon/Projects/Rust/hellocargo/target/debug/deps/hellocargo-f5b6d01e6c1e54a6.rmeta: src/main.rs
+
+/Users/suhokwon/Projects/Rust/hellocargo/target/debug/deps/hellocargo-f5b6d01e6c1e54a6.d: src/main.rs
+
+src/main.rs:
