6cbfc8394c2c733412130224a9498738aa44d25a
Suho Kwon <34533493+knix008@users.noreply.github.com>
2022-07-03 15:04:04 +0900

    Delete comments/target directory

--- comments/target/debug/comments.d
+++
@@ -1,1 +0,0 @@
-/Users/suhokwon/Projects/Rust/comments/target/debug/comments: /Users/suhokwon/Projects/Rust/comments/src/main.rs
--- comments/target/debug/deps/comments-7a428badbfa97c56.d
+++
@@ -1,5 +0,0 @@
-/Users/suhokwon/Projects/Rust/comments/target/debug/deps/comments-7a428badbfa97c56: src/main.rs
-
-/Users/suhokwon/Projects/Rust/comments/target/debug/deps/comments-7a428badbfa97c56.d: src/main.rs
-
-src/main.rs:
