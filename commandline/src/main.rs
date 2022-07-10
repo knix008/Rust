c47b7d6a12171252c7377903939146a2d09fb6ba
Suho Kwon <34533493+Knix008@users.noreply.github.com>
2022-07-10 16:52:54 +0900

    Add commandline example.

--- commandline/src/main.rs
+++ commandline/src/main.rs
@@ -1,6 +1,19 @@
 use std::env;
 use std::str::FromStr;
 
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
 fn main() {
     let mut numbers = Vec::new();
     for arg in env::args().skip(1) {
