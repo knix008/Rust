5a5784e35add9ccdbe1ca4723a7c0346dde3a363
Suho Kwon <suho.kwon@gmail.com>
2022-12-25 16:44:05 +0900

    Add more examples.

---
+++ Part01/scope03/src/main.rs
@@ -0,0 +1,10 @@
+fn scope() {
+    let s = String::from("hello"); // s is valid from this point forward
+    // do stuff with s
+    println!("{}", s);
+} // this scope is
+
+fn main() {
+    scope();
+    println!("End of the main function.");
+}
