800e2ccc9e94749cbfbb489337d62b549333c9a0
Suho Kwon <suho.kwon@gmail.com>
2023-06-28 13:54:45 +0900

    Add more examples.

---
+++ Part02/struct_definition02/src/main.rs
@@ -0,0 +1,22 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn main() {
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    
+    user1.email = String::from("anotheremail@example.com");
+
+    println!("name : {}", user1.username);
+    println!("email : {}", user1.email);
+    println!("active : {}", user1.active);
+    println!("sign_in_count : {}", user1.sign_in_count);
+}
