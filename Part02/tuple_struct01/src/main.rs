a0a66542ce0a0cec6fd127dff543e3066ccd5958
Suho Kwon <suho.kwon@gmail.com>
2023-06-29 11:41:43 +0900

    Add more examples.

--- Part02/tuple_struct01/src/main.rs
+++ Part02/tuple_struct01/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 1, 2);
+    let origin = Point(3, 4, 5);
+
+    println!("{}", black.0);
+    println!("{}", black.1);
+    println!("{}", black.2);
+    println!("{}", origin.0);
+    println!("{}", origin.1);
+    println!("{}", origin.2);
 }
