24d6a7c52a8540093e8bf057037273934704e079
Suho Kwon <suho.kwon@gmail.com>
2023-07-04 16:56:57 +0900

    Add more examples.

---
+++ Part02/match02/src/main.rs
@@ -0,0 +1,25 @@
+#![allow(dead_code)]
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn main() {
+    let coin = Coin::Penny;
+    println!("The match coin : {}", value_in_cents(coin));
+}
