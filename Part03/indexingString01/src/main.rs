43ce1c3988a217d8585ac8205e21c564e58f4afb
Suho Kwon <suho.kwon@gmail.com>
2023-07-11 14:55:31 +0900

    Add more examples.

---
+++ Part03/indexingString01/src/main.rs
@@ -0,0 +1,10 @@
+#![allow(unused_mut)]
+#![allow(unused_variables)]
+#![allow(non_snake_case)]
+
+fn main() {
+    let s1 = String::from("hello");
+    let h = s1[0];
+
+    println!("The string : {}", h);
+}
---
+++ Part03/indexingString02/src/main.rs
@@ -0,0 +1,14 @@
+#![allow(unused_mut)]
+#![allow(unused_variables)]
+#![allow(non_snake_case)]
+
+fn main() {
+    let len = String::from("Hola").len();
+    println!("1. The string length : {}", len);
+    let len = String::from("Здравствуйте").len();
+    println!("1. The string length : {}", len);
+
+    let hello = "Здравствуйте";
+    //let answer = &hello[0];
+    //println!("The answer : {}", answer);
+}
