d00ae8b9521aee61e744716ccc9c4b80fc2a62ca
Suho Kwon <suho.kwon@gmail.com>
2023-07-14 10:14:54 +0900

    Add more examples.

---
+++ Part03/propagatingErrors02/src/main.rs
@@ -0,0 +1,19 @@
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+
+use std::io;
+use std::io::Read;
+use std::fs::File;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn main() {
+    let result = read_username_from_file();
+    println!("The result : {:?}", result);
+}
