cd621bb1427e026f5cb06f9a246b20d0307cdc1e
Suho Kwon <suho.kwon@gmail.com>
2023-07-17 00:19:24 +0900

    Add more examples.

--- Part04/findingLargest03/src/main.rs
+++ Part04/findingLargest03/src/main.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
 
-fn largest<T>(list: &[T]) -> T {
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> T where T : Copy {
     let mut largest = list[0];
     for &item in list.iter() {
         if item > largest {
@@ -14,7 +14,7 @@ fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
     let result = largest(&number_list);
     println!("The largest number is {}", result);
-    
+
     let char_list = vec!['y', 'm', 'a', 'q'];
     let result = largest(&char_list);
     println!("The largest char is {}", result);
