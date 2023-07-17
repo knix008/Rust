d3313a57139c6e2cc9d63774e36da438c9c58cca
Suho Kwon <suho.kwon@gmail.com>
2023-07-17 14:30:02 +0900

    Add more examples.

---
+++ Part04/monomorphization01/src/main.rs
@@ -0,0 +1,22 @@
+#![allow(non_camel_case_types)]
+#![allow(dead_code)]
+
+#[derive(Debug)]
+enum Option_i32 {
+    Some(i32),
+    None,
+}
+
+#[derive(Debug)]
+enum Option_f64 {
+    Some(f64),
+    None,
+}
+
+fn main() {
+    let integer = Option_i32::Some(5);
+    let float = Option_f64::Some(5.0);
+
+    println!("The integer version : {:?}", integer);
+    println!("The float version : {:?}", float);
+}
