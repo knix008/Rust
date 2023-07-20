ce8e9dd274e94c36d75ef721d675557a6da894fe
Suho Kwon <suho.kwon@gmail.com>
2023-07-20 17:57:34 +0900

    Add more examples.

--- Part04/allInOne/src/main.rs
+++ Part04/allInOne/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(non_snake_case)]
 use std::fmt::Display;
 
 fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str where T: Display {
@@ -7,4 +8,13 @@ fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a st
     } else {
         y
     }
+}
+
+fn main() {
+    let string1 = String::from("long string is long");
+    let string2 = String::from("xyz");
+    let announcement = "Ladies and Gentleman!!!";
+    
+    let result = longest_with_an_announcement(string1.as_str(), string2.as_str(), announcement);
+    println!("The longest string is : {}.", result);
 }
