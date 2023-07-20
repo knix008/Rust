1c544186dfa3cb5be18576e71be519eb9c49a390
Suho Kwon <suho.kwon@gmail.com>
2023-07-20 16:55:50 +0900

    Add more examples.

---
+++ Part04/lifetime16/src/main.rs
@@ -0,0 +1,11 @@
+fn longest<'a, 'b>(x: &'a str, y: &'b str) -> &str {
+    if x.len() > y.len() { x } else { y }
+}
+
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+    let result = longest(string1.as_str(), string2);
+
+    println!("The longest string is {}", result);
+}
---
+++ Part04/lifetime18/src/main.rs
@@ -0,0 +1,33 @@
+#![allow(dead_code)]
+#![allow(non_snake_case)]
+
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let excerpt = ImportantExcerpt { part: first_sentence };
+    let level = excerpt.level();
+
+    println!("The excerpt : {:?}.", excerpt);
+    println!("The level : {}.", level);
+
+    let announce = excerpt.announce_and_return_part(&first_sentence);
+    println!("The announce : {}.", announce);
+}
