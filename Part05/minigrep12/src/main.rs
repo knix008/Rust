607157d262ebe8308aa408dd1efe4c8023ffe3f8
Suho Kwon <suho.kwon@gmail.com>
2023-07-26 17:40:40 +0900

    Add more examples.

--- Part05/minigrep12/src/main.rs
+++ Part05/minigrep12/src/main.rs
@@ -1,17 +1,24 @@
+#![allow(unused_must_use)]
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
-
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
+    run(config);
+}
 
-    let contents = fs
-        ::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
     println!("With text:\n{}", contents);
+    Ok(())
 }
 
 struct Config {
@@ -26,7 +33,6 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-        
         Ok(Config { query, filename })
     }
 }
