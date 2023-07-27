fa320a197b28fc9478bf8c72a17372a3a7113289
Suho Kwon <suho.kwon@gmail.com>
2023-07-27 13:47:00 +0900

    Add more examples.

---
+++ Part05/tdd03/src/lib.rs
@@ -0,0 +1,20 @@
+#![allow(unused_variables)]
+
+pub fn search(query: &str, contents: & str) -> Vec<& str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
