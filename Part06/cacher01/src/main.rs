5a366d8a0a430996c674ce3f02787c4c36771a1b
Suho Kwon <suho.kwon@gmail.com>
2023-07-29 12:32:55 +0900

    Add more examples.

---
+++ Part06/cacher01/src/main.rs
@@ -0,0 +1,32 @@
+struct Cacher<T> where T: Fn(u32) -> u32 {
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T> where T: Fn(u32) -> u32 {
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
+fn main() {
+    println!("The Cacher Example.");
+    let cal = |x| -> u32 { x };
+    let mut cacher = Cacher::new(cal);
+
+    println!("The cacher : {}.", cacher.value(100));
+}
