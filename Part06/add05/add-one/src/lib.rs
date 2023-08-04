0526dede7fc22ba50d64e1b28def4970de28cf17
Suho Kwon <suho.kwon@gmail.com>
2023-08-04 17:08:17 +0900

    Add more examples.

---
+++ Part06/add05/add-one/src/lib.rs
@@ -0,0 +1,16 @@
+#![allow(unused_imports)]
+use rand;
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add_one(1);
+        assert_eq!(result, 2);
+    }
+}
---
+++ Part06/add06/adder/src/main.rs
@@ -0,0 +1,8 @@
+#![allow(unused_imports)]
+use rand;
+use add_one;
+
+fn main() {
+    let num = 10;
+    println!("Hello, world! {} plus one is {}!", num, add_one::add_one(num));
+}
