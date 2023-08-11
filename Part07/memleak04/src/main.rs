98e81710c6ea5c084faf5732743fefc812a09ca4
Suho Kwon <suho.kwon@gmail.com>
2023-08-11 12:47:45 +0900

    Add more examples.

---
+++ Part07/memleak04/src/main.rs
@@ -0,0 +1,24 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+use std::rc::Rc;
+use std::cell::RefCell;
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    children: RefCell<Vec<Rc<Node>>>,
+}
+
+fn main() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        children: RefCell::new(vec![]),
+    });
+    let branch = Rc::new(Node {
+        value: 5,
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+
+    println!("The leaf : {:?}.", leaf);
+    println!("The branch : {:?}.", branch);
+}
