de9000b826bbef535fb8f94da7d9195dd0cf3644
Suho Kwon <suho.kwon@gmail.com>
2023-08-14 16:25:12 +0900

    Add more examples.

---
+++ Part07/channel02/src/main.rs
@@ -0,0 +1,14 @@
+#![allow(unused_variables)]
+use std::thread;
+use std::sync::mpsc;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+    
+    println!("Done!!!");
+}
