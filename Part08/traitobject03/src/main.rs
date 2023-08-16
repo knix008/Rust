616bd62c18254fb0b4333ad1239c10cf78f13463
Suho Kwon <suho.kwon@gmail.com>
2023-08-16 18:04:56 +0900

    Add more examples.

---
+++ Part08/traitobject03/src/main.rs
@@ -0,0 +1,32 @@
+#![allow(dead_code)]
+use gui::Draw;
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // code to actually draw a button
+        println!("Drawing button!!! : {}.", self.label);
+    }
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+        println!("Drawing selectbox!!!.");
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
