5870faebf6acadd472e15fcf6ad672f584979019
Suho Kwon <suho.kwon@gmail.com>
2023-08-17 11:05:04 +0900

    Add more examples.

---
+++ Part08/traitobject05/src/main.rs
@@ -0,0 +1,34 @@
+#![allow(dead_code)]
+use gui::Draw;
+use gui::{ Screen, Button };
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+        println!("Drawing selectbox!!!");
+    }
+}
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![String::from("Yes"), String::from("Maybe"), String::from("No")],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            })
+        ],
+    };
+    screen.run();
+}
