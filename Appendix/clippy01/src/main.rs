9709e33a18bda3776162a8c8f700f11607f5b5b7
Suho Kwon <suho.kwon@gmail.com>
2023-09-04 16:41:47 +0900

    Add more examples.

---
+++ Appendix/clippy01/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let x = 3.1415;
+    let r = 8.0;
+    
+    println!("the area of the circle is {}", x * r * r);
+}
---
+++ Appendix/clippy02/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let x = std::f64::consts::PI;
+    let r = 8.0;
+    
+    println!("the area of the circle is {}", x * r * r);
+}
